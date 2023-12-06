6f27460c3b1f51fc7105921382acb32bacdd919c
Ruan Moolman <ruan@renderheads.com>
2023-12-06 17:37:34 +0200

    Day 5 Part 2

--- day5/day5.go
+++ day5/day5.go
@@ -1,8 +1,11 @@
 package day5
 
 import (
+	"cmp"
 	"fmt"
+	intmath "github.com/thomaso-mirodin/intmath/intgr"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -14,16 +17,36 @@ var example string
 //go:embed input.txt
 var input string
 
-type Range struct {
-	destination int
+type MappingRange struct {
 	source      int
+	destination int
 	count       int
+}
+
+func (r MappingRange) String() string {
+	return fmt.Sprintf("[%d, %d)->[%d, %d)", r.source, r.sourceEnd(), r.destination, r.destEnd())
+}
+
+func (r MappingRange) diff() int {
+	return r.destination - r.source
+}
+
+func (r MappingRange) sourceRangeContains(value int) bool {
+	return value >= r.source && value < r.sourceEnd()
+}
+
+func (r MappingRange) sourceEnd() int {
+	return r.source + r.count
+}
+
+func (r MappingRange) destEnd() int {
+	return r.destination + r.count
 }
 
 type Mapping struct {
 	from   string
 	to     string
-	ranges []Range
+	ranges []MappingRange
 }
 
 var mapStartRegex, _ = regexp.Compile(`([a-z]+)-to-([a-z]+) map:`)
@@ -36,9 +59,9 @@ func mustAtoi(in string) int {
 	return val
 }
 
-func parseRange(input string) Range {
+func parseRange(input string) MappingRange {
 	parts := strings.Split(input, " ")
-	return Range{
+	return MappingRange{
 		destination: mustAtoi(parts[0]),
 		source:      mustAtoi(parts[1]),
 		count:       mustAtoi(parts[2]),
@@ -70,6 +93,9 @@ func parseMappings(input string) []Mapping {
 			}
 			m.ranges = append(m.ranges, parseRange(line))
 		}
+		slices.SortFunc(m.ranges, func(a, b MappingRange) int {
+			return cmp.Compare(a.source, b.source)
+		})
 		mm = append(mm, m)
 	}
 	return mm
@@ -77,7 +103,7 @@ func parseMappings(input string) []Mapping {
 
 type seedMap map[string]int
 
-func mapID(id int, ranges []Range) int {
+func mapID(id int, ranges []MappingRange) int {
 	for _, r := range ranges {
 		if id >= r.source && id < r.source+r.count {
 			return r.destination + (id - r.source)
@@ -103,36 +129,112 @@ func mapSeeds(seeds []int, mappings []Mapping) []seedMap {
 	return mm
 }
 
-func parseSeedsPart2(input string) []int {
+type IDRange struct {
+	start int
+	count int
+}
+
+func (r IDRange) String() string {
+	return fmt.Sprintf("[%d, %d)", r.start, r.end())
+}
+
+func (r IDRange) end() int {
+	return r.start + r.count
+}
+
+func parseSeedRanges(input string) []IDRange {
 	line := strings.Split(input, "\n")[0]
 	vvStr := strings.Split(strings.Trim(strings.Split(line, ":")[1], " "), " ")
-	var vv []int
+	var srs []IDRange
 	for i := 0; i < len(vvStr); i += 2 {
-		from := mustAtoi(vvStr[i])
-		count := mustAtoi(vvStr[i+1])
-		for s := from; s < from+count; s++ {
-			vv = append(vv, s)
+		srs = append(srs, IDRange{
+			start: mustAtoi(vvStr[i]),
+			count: mustAtoi(vvStr[i+1]),
+		})
+	}
+
+	slices.SortFunc(srs, func(a, b IDRange) int {
+		return cmp.Compare(a.start, b.start)
+	})
+	return srs
+}
+
+func rangeForIndex(mapping Mapping, index int) (MappingRange, bool) {
+	for _, r := range mapping.ranges {
+		if r.sourceRangeContains(index) {
+			return r, true
 		}
 	}
-	return vv
+	return MappingRange{}, false
+}
+func nextRangeAfterIndex(mapping Mapping, index int) (MappingRange, bool) {
+	for _, r := range mapping.ranges {
+		if r.source >= index {
+			return r, true
+		}
+	}
+	return MappingRange{}, false
+}
+
+func applyMappingToRange(idRange IDRange, mapping Mapping) []IDRange {
+	var rr []IDRange
+	i := idRange.start
+	for i < idRange.end() {
+		if r, ok := rangeForIndex(mapping, i); ok {
+			d := r.diff()
+			count := intmath.Min(r.sourceEnd(), idRange.end()) - i
+			rr = append(rr, IDRange{start: i + d, count: count})
+			i += count
+		} else {
+			if nextR, ok := nextRangeAfterIndex(mapping, i); ok {
+				count := intmath.Min(nextR.source, idRange.end()) - i
+				rr = append(rr, IDRange{start: i, count: count})
+				i += count
+			} else {
+				newRange := IDRange{start: i, count: idRange.end() - i}
+				rr = append(rr, newRange)
+				i += newRange.count
+			}
+		}
+	}
+	return rr
+}
+
+func applyMappingsToRange(idRange IDRange, mappings []Mapping) []IDRange {
+	rr := []IDRange{idRange}
+	for _, m := range mappings {
+		var rrNew []IDRange
+		for _, r := range rr {
+			rrNew = append(rrNew, applyMappingToRange(r, m)...)
+		}
+		rr = rrNew
+	}
+	return rr
+}
+
+func mapIDRanges(rr []IDRange, mappings []Mapping) []IDRange {
+	var mm []IDRange
+	for _, s := range rr {
+		mm = append(mm, applyMappingsToRange(s, mappings)...)
+	}
+	return mm
 }
 
 func Execute() {
 	inputString := input
-	seeds := parseSeedsPart2(inputString)
+	seeds := parseSeedRanges(inputString)
 	mm := parseMappings(inputString)
-	ss := mapSeeds(seeds, mm)
+	rr := mapIDRanges(seeds, mm)
 	ans := -1
-	for _, s := range ss {
-		locationID := s["location"]
-		if locationID < ans || ans == -1 {
-			ans = locationID
+	for _, s := range rr {
+		if s.start < ans || ans == -1 {
+			ans = s.start
 		}
 	}
 	fmt.Printf("answer %d\n", ans)
 }
 
-func parseSeedsPart1(input string) []int {
+func parseSeeds(input string) []int {
 	line := strings.Split(input, "\n")[0]
 	valuesString := strings.Trim(strings.Split(line, ":")[1], " ")
 	var vv []int
@@ -144,7 +246,7 @@ func parseSeedsPart1(input string) []int {
 
 func ExecutePart1() {
 	inputString := input
-	seeds := parseSeedsPart1(inputString)
+	seeds := parseSeeds(inputString)
 	mm := parseMappings(inputString)
 	ss := mapSeeds(seeds, mm)
 	ans := -1
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module ruan.moolman/aoc2023
 
-go 1.20
+go 1.21
 
 require github.com/thomaso-mirodin/intmath v0.0.0-20160323211736-5dc6d854e46e
