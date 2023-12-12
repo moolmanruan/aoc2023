18eef09a4effc375f0ea4c80538d15dc1f19118d
Ruan Moolman <ruan@renderheads.com>
2023-12-12 23:46:54 +0200

    Day 12 Cleanup

--- day12/main.go
+++ day12/main.go
@@ -2,7 +2,7 @@ package day12
 
 import (
 	_ "embed"
-	"gonum.org/v1/gonum/stat/combin"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -13,6 +13,18 @@ var input string
 type springReport struct {
 	report string
 	groups []int
+}
+
+func Part1() string {
+	res := process(1)
+	fmt.Println("Part 1: 7705")
+	return res
+}
+
+func Part2() string {
+	res := process(5)
+	fmt.Println("Part 2: 50338344809230")
+	return res
 }
 
 func parseSpringReport(line string) springReport {
@@ -28,73 +40,124 @@ func parseSpringReport(line string) springReport {
 	return springReport{report: parts[0], groups: groups}
 }
 
-func sum(vv []int) int {
-	var r int
-	for _, v := range vv {
-		r += v
-	}
-	return r
-}
-
-func groupsAdditional(groups []int) int {
-	return sum(groups) - len(groups)
-}
-
-func generateReport(size int, combination, groups []int) string {
-	var c int
-	var out string
-	for ci, combo := range combination {
-		for c < combo {
-			out += "."
-			c++
-		}
-		for i := 0; i < groups[ci]; i++ {
-			out += "#"
-		}
-	}
-	for len(out) < size {
-		out += "."
-	}
-	return out[:size]
-}
-
-func valid(report, variation string) bool {
-	if len(report) != len(variation) {
-		panic("variation and report size mismatch")
-	}
-	for i := 0; i < len(report); i++ {
-		if report[i] == '?' {
-			continue
-		}
-		if report[i] != variation[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func numVariations(report springReport) int {
-	additional := groupsAdditional(report.groups)
-	gaps := len(report.groups) - 1
-	maxLen := len(report.report) - gaps - additional
-	cc := combin.Combinations(maxLen, len(report.groups))
-	s := len(report.report)
-	var count int
-	for _, combo := range cc {
-		if valid(report.report, generateReport(s, combo, report.groups)) {
-			count++
-		}
-	}
-	return count
-}
-
-func Part1() string {
+func process(folds int) string {
 	lines := strings.Split(input, "\n")
 	var ans int
-	for _, line := range lines {
+	numLines := len(lines)
+	for li, line := range lines {
+		fmt.Printf("%d/%d\n", li, numLines)
 		sp := parseSpringReport(line)
-		nv := numVariations(sp)
-		ans += nv
+		sp = unfold(sp, folds)
+		ans += countVariations(sp.report, sp.groups)
 	}
 	return strconv.Itoa(ans)
+}
+
+func unfold(sp springReport, numFolds int) springReport {
+	var newReportLines []string
+	var newGroups []int
+	for i := 0; i < numFolds; i++ {
+		newReportLines = append(newReportLines, sp.report)
+		newGroups = append(newGroups, sp.groups...)
+	}
+	return springReport{report: strings.Join(newReportLines, "?"), groups: newGroups}
+}
+
+const gap = 1
+
+func leftString(s string, i int) string {
+	if i <= 0 {
+		return ""
+	}
+	return s[:i]
+}
+func rightString(s string, i int) string {
+	if i >= len(s) {
+		return ""
+	}
+	return s[i:]
+}
+
+func leftIntSlice(s []int, i int) []int {
+	if i <= 0 {
+		return nil
+	}
+	return s[:i]
+}
+func rightIntSlice(s []int, i int) []int {
+	if i >= len(s) {
+		return nil
+	}
+	return s[i:]
+}
+
+func countVariations(report string, groups []int) int {
+	if len(groups) > 0 && len(report) == 0 {
+		// invalid if we still have groups to place, but no space left
+		return 0
+	}
+	if len(groups) == 0 {
+		if len(report) == 0 {
+			// if we have no space, but now groups as well, this is valid
+			return 1
+		} else if !strings.Contains(report, "#") {
+			// if there are no forced placements left, this is valid
+			return 1
+		} else {
+			// out of groups, but still need to place, thus invalid
+			return 0
+		}
+	}
+
+	// get the middle group to split the remaining groups
+	groupIndex := len(groups) / 2
+	group := groups[groupIndex]
+
+	var branches int
+	// for each valid position of the group, count up all the variations
+	for _, p := range groupValidPos(report, group) {
+		// left
+		lC := countVariations(leftString(report, p-gap), leftIntSlice(groups, groupIndex))
+		if lC == 0 {
+			// don't do the right hand side if this is already 0
+			continue
+		}
+		// right
+		rC := countVariations(rightString(report, p+group+gap), rightIntSlice(groups, groupIndex+1))
+		branches += lC * rC
+	}
+	return branches
+}
+
+func groupValidPos(report string, size int) []int {
+	var pp []int
+	if size > len(report) {
+		return pp
+	}
+	last := len(report) - size
+
+positionLoop:
+	for i := 0; i <= last; i++ {
+		// should be open before the group
+		if i > 0 {
+			if report[i-1] == '#' {
+				continue positionLoop
+			}
+		}
+		// should have values for each spot in the group
+		for j := 0; j < size; j++ {
+			if report[i+j] == '.' {
+				continue positionLoop
+			}
+		}
+		// should be open after the group
+		iLast := i + size
+		if iLast < len(report) {
+			if report[iLast] == '#' {
+				continue positionLoop
+			}
+		}
+		pp = append(pp, i)
+	}
+	return pp
 }
--- day12/main_test.go
+++ day12/main_test.go
@@ -5,108 +5,61 @@ import (
 	"testing"
 )
 
-func TestNumVariations(t *testing.T) {
+func TestCountVariations(t *testing.T) {
 	cases := []struct {
 		name   string
-		report springReport
+		input  string
+		groups []int
 		want   int
 	}{
 		{
-			"only option",
-			springReport{"?", []int{1}},
+			"single with single option",
+			"?",
+			[]int{1},
 			1,
 		},
 		{
-			"two options",
-			springReport{"??", []int{1}},
+			"single with two option",
+			"??",
+			[]int{1},
 			2,
 		},
 		{
-			"two groups",
-			springReport{"???", []int{1, 1}},
+			"single with three option",
+			"???",
+			[]int{1},
+			3,
+		},
+		{
+			"double multi",
+			"??.??",
+			[]int{1, 1},
+			4,
+		},
+		{
+			"example line 2",
+			".??..??...?##.",
+			[]int{1, 1, 3},
+			4,
+		},
+		{
+			"line 993",
+			".##????###??#..#????",
+			[]int{10, 1, 3},
 			1,
 		},
 		{
-			"two bigger groups",
-			springReport{"??????", []int{2, 3}},
-			1,
-		},
-		{
-			"with fixed values",
-			springReport{"#.??", []int{1, 1}},
-			2,
+			"less groups than needed",
+			"#.#",
+			[]int{1},
+			0,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			assert.Equal(t, c.want, numVariations(c.report))
-		})
-	}
-}
-
-func TestGenerateReport(t *testing.T) {
-	cases := []struct {
-		name  string
-		size  int
-		combo []int
-		group []int
-		want  string
-	}{
-		{
-			"single a",
-			2,
-			[]int{0},
-			[]int{1},
-			"#.",
-		},
-		{
-			"single b",
-			2,
-			[]int{1},
-			[]int{1},
-			".#",
-		},
-		{
-			"double a",
-			6,
-			[]int{0, 1},
-			[]int{2, 2},
-			"##.##.",
-		},
-		{
-			"double b",
-			6,
-			[]int{0, 2},
-			[]int{2, 2},
-			"##..##",
-		},
-		{
-			"double c",
-			6,
-			[]int{1, 2},
-			[]int{2, 2},
-			".##.##",
-		},
-		{
-			"multi a",
-			12,
-			[]int{0, 1, 2},
-			[]int{2, 3, 2},
-			"##.###.##...",
-		},
-		{
-			"multi b",
-			12,
-			[]int{1, 3, 4},
-			[]int{2, 3, 2},
-			".##..###.##.",
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			assert.Equal(t, c.want, generateReport(c.size, c.combo, c.group))
+			got := countVariations(c.input, c.groups)
+			assert.Equal(t, c.want, got)
 		})
 	}
 }
